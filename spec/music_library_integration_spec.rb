4bb9821de200a54b31e1229e9bf7135b5e736053
Finn McCoy <finnmccoy99@gmail.com>
2022-12-12 11:56:17 +0000

    Added integration tests for search method

--- spec/music_library_integration_spec.rb
+++ spec/music_library_integration_spec.rb
@@ -2,15 +2,21 @@ require "music_library"
 
 RSpec.describe "MusicLibrary integration testing" do
   context "With tracks added to music library" do
+    lib = MusicLibrary.new
+    track1 = Track.new("great song", "great band")
+    track2 = Track.new("bad song", "bad band")
     it "all returns saved tracks in array" do
-      lib = MusicLibrary.new
-      track1 = Track.new("great_song", "great_band")
-      track2 = Track.new("bad_song", "bad_band")
       lib.add(track1)
       expect(lib.all).to eq [track1]
       lib.add(track2)
       expect(lib.all).to eq [track1, track2]
     end
     
+    it "search returns saved tracks that match keyword in array" do
+      expect(lib.search("song")).to eq [track1, track2]
+      expect(lib.search("band")).to eq [track1, track2]
+      expect(lib.search("great")).to eq [track1]
+      expect(lib.search("bad")). to eq [track2]
+    end
   end
 end
