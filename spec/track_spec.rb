4344d86e836a5e2ad2dc0690bcf2d19f8a3f7075
Finn McCoy <finnmccoy99@gmail.com>
2022-12-12 11:00:43 +0000

    Added more tests for matches? method

--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe Track do
   end
   
   context "matches? called with string argument" do
+    track1 = Track.new("California Girls", "Katy Perry")
+    track2 = Track.new("Wannabe", "The Spice Girls")
     it "returns true if string matches either title or artist" do
-      track1 = Track.new("California Girls", "Katy Perry")
-      track2 = Track.new("Wannabe", "The Spice Girls")
       expect(track1.matches?("Girls")).to eq true
+      expect(track2.matches?("Girls")).to eq true
+    end
+    
+    it "returns false if string does not match either title or artist" do
+      expect(track1.matches?("Gurls")).to eq false
+      expect(track2.matches?("Spicy")).to eq false
     end
   end
 end
