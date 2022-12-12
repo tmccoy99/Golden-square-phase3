8f2e356e7b77dac6b6c78c236bc1f1a986f9e932
Finn McCoy <finnmccoy99@gmail.com>
2022-12-12 10:57:27 +0000

    Added contents to initalize and matches? methods

--- lib/track.rb
+++ lib/track.rb
@@ -1,8 +1,10 @@
 class Track
   def initialize(title, artist) # title and artist are both strings
+  @title = title ; @artist = artist
+  @title_words = @title.split ; @artist_words = @artist.split
   end
 
-  def matches?(keyword) # keyword is a string
-    # Returns true if the keyword matches either the title or artist
+  def matches?(keyword)
+    (@title_words + @artist_words).include?(keyword)
   end
 end
