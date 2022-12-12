06a475aa1e4514d49666840874256ee58af98b96
Finn McCoy <finnmccoy99@gmail.com>
2022-12-12 11:24:33 +0000

    Added unit test for all method with saved tracks using a double

--- spec/music_library_spec.rb
+++ spec/music_library_spec.rb
@@ -10,5 +10,21 @@ RSpec.describe MusicLibrary do
     it "returns empty array on calls of all" do
       expect(empty_lib.all).to eq []
     end
+  
+  
+    it "returns empty array on calls of search" do
+      expect(empty_lib.search("Spice")).to eq []
+    end
+    
+  end
+  
+  context "When library contains tracks" do
+    lib = MusicLibrary.new
+    it "all method returns array containing saved tracks" do
+      dbl = double("Fake track", matches?: true)
+      lib.add(dbl)
+      expect(lib.all).to eq [dbl]
+    end
+    
   end
 end
