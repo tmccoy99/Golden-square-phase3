5c72968c5cb4bce7e9059909a5d75d4d91953c6d
Finn McCoy <finnmccoy99@gmail.com>
2022-12-13 10:41:55 +0000

    Wrote integration test for calling #read after #unlock

--- spec/secret_diary_integration_spec.rb
+++ spec/secret_diary_integration_spec.rb
@@ -11,5 +11,13 @@ describe "SecretDiary integration testing:" do
       expect{ secret_diary.read }.to raise_error "Go away!"
     end
   end
+  
+  context "#read is called after #unlock is called on locked instance" do
+    it "returns @contents of Diary instance" do
+      secret_diary =  SecretDiary.new(Diary.new("contents"))
+      secret_diary.unlock
+      expect(secret_diary.read).to eq "contents"
+    end
+  end
 end
 
